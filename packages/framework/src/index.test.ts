fc7e8dded1879fb5bf182dcffa68b5d259e2b4c3
caio2983 <caioluna2002@gmail.com>
2024-10-26 20:52:48 -0300

    chore: migrate framework package from uvu to vitest

--- packages/framework/src/index.test.ts
+++ packages/framework/src/index.test.ts
@@ -1,5 +1,4 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+import { test, expect } from 'vitest'
 
 import * as async from '@reatom/async'
 import * as core from '@reatom/core'
@@ -10,23 +9,12 @@ import * as logger from '@reatom/logger'
 import * as primitives from '@reatom/primitives'
 import * as utils from '@reatom/utils'
 
-test(`base API`, async () => {
-  const packages = [
-    async,
-    core,
-    effects,
-    hooks,
-    lens,
-    logger,
-    primitives,
-    utils,
-  ]
+test('base API', async () => {
+  const packages = [async, core, effects, hooks, lens, logger, primitives, utils]
 
   const allExports = packages
     .reduce((acc, v) => [...acc, ...Object.keys(v)], new Array<string>())
     .filter((name) => name !== 'default')
 
-  assert.equal(allExports, [...new Set(allExports)])
+  expect(allExports).toEqual([...new Set(allExports)])
 })
-
-test.run()
