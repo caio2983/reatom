ecad5c4c6d98e0175a4b123db2fd06b18e932b48
caio2983 <caioluna2002@gmail.com>
2024-10-27 01:57:29 -0300

    chore: migrate form-web package from uvu to vitest

--- packages/form-web/src/index.test.ts
+++ packages/form-web/src/index.test.ts
@@ -1,10 +1,8 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+import { describe, test, expect } from 'vitest'
 
-import {} from './'
-
-test(`base API`, async () => {
-  // assert.ok(false, `You forgot test you code`)
+describe('Base API', () => {
+  test('base API', async () => {
+    // expect.fail('You forgot to test your code')
+    expect(true).toBe(true) // Placeholder for an actual test
+  })
 })
-
-test.run()
