c26824307f3ee079b2a2b1258ca84ada047b3792
caio2983 <caioluna2002@gmail.com>
2024-10-28 12:45:22 -0300

    refactor(async): remove describe usage

--- packages/async/src/mapToAsync.test.ts
+++ packages/async/src/mapToAsync.test.ts
@@ -1,52 +1,50 @@
-import { describe, it, expect } from 'vitest'
+import { it, expect } from 'vitest'
 import { take, takeNested } from '@reatom/effects'
 import { createTestCtx } from '@reatom/testing'
 import { atom } from '@reatom/core'
 import { mapToAsync, withDataAtom } from './index'
 
-describe('mapToAsync', () => {
-  it(`mapToAsync interface`, () => {
-    const argumentAtom = atom(0, 'argumentAtom')
-    const asyncAction = argumentAtom.pipe(mapToAsync(async (ctx, arg) => arg))
+test(`mapToAsync interface`, () => {
+  const argumentAtom = atom(0, 'argumentAtom')
+  const asyncAction = argumentAtom.pipe(mapToAsync(async (ctx, arg) => arg))
 
-    expect(typeof asyncAction).toBe('function')
-    expect(asyncAction.__reatom.name).toBe('argumentAtom.mapToAsync')
-    expect(typeof asyncAction.unstable_unhook).toBe('function')
-    ;`👍` //?
-  })
+  expect(typeof asyncAction).toBe('function')
+  expect(asyncAction.__reatom.name).toBe('argumentAtom.mapToAsync')
+  expect(typeof asyncAction.unstable_unhook).toBe('function')
+  ;`👍` //?
+})
 
-  it(`is called whenever argument is changed`, async () => {
-    const argumentAtom = atom('initial', 'argumentAtom')
-    const asyncAction = argumentAtom.pipe(
-      mapToAsync(async (ctx, arg) => arg),
-      withDataAtom('default'),
-    )
-    const ctx = createTestCtx()
+test(`is called whenever argument is changed`, async () => {
+  const argumentAtom = atom('initial', 'argumentAtom')
+  const asyncAction = argumentAtom.pipe(
+    mapToAsync(async (ctx, arg) => arg),
+    withDataAtom('default'),
+  )
+  const ctx = createTestCtx()
 
-    expect(ctx.get(asyncAction.dataAtom)).toBe('default')
+  expect(ctx.get(asyncAction.dataAtom)).toBe('default')
 
-    const hijackedCall = take(ctx, asyncAction)
+  const hijackedCall = take(ctx, asyncAction)
 
-    argumentAtom(ctx, 'updated')
+  argumentAtom(ctx, 'updated')
 
-    expect(await hijackedCall).toBe('updated')
-    expect(ctx.get(asyncAction.dataAtom)).toBe('updated')
-    ;`👍` //?
-  })
+  expect(await hijackedCall).toBe('updated')
+  expect(ctx.get(asyncAction.dataAtom)).toBe('updated')
+  ;`👍` //?
+})
 
-  it(`can be unhooked`, async () => {
-    const argumentAtom = atom('initial', 'argumentAtom')
-    const asyncAction = argumentAtom.pipe(
-      mapToAsync(async (ctx, n) => n),
-      withDataAtom('default'),
-    )
+test(`can be unhooked`, async () => {
+  const argumentAtom = atom('initial', 'argumentAtom')
+  const asyncAction = argumentAtom.pipe(
+    mapToAsync(async (ctx, n) => n),
+    withDataAtom('default'),
+  )
 
-    asyncAction.unstable_unhook()
+  asyncAction.unstable_unhook()
 
-    const ctx = createTestCtx()
+  const ctx = createTestCtx()
 
-    await takeNested(ctx, argumentAtom, 'updated')
-    expect(ctx.get(asyncAction.dataAtom)).toBe('default')
-    ;`👍` //?
-  })
+  await takeNested(ctx, argumentAtom, 'updated')
+  expect(ctx.get(asyncAction.dataAtom)).toBe('default')
+  ;`👍` //?
 })
